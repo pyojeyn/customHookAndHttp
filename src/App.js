293a97adb6acf32fda0a9372c9aba26ae0c4b979
pyojeyn <jeynpyo@gmail.com>
2023-06-28 22:40:17 +0900

    사용자 정의 HTTP HOOK 빌드하기

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transfromTasks = tasksObj => {
+    const loadedTasks = [];
+
+    for(const taskKey in tasksObj){
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+    setTasks(loadedTasks);
+  };
+
+  
+ 
+  const { isLoading, error, sendRequest: fetchTasks} = useHttp({
+    url: 'https://react-http-e83a8-default-rtdb.firebaseio.com/tasks.json',
+  }, transfromTasks);
+
+  
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
